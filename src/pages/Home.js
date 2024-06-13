9a48ab48fc52953a1df93905613bb3aa414e19e2
Aditya Nama <92026058+adityanama@users.noreply.github.com>
2024-06-13 18:17:23 +0530

    ok

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+    return (
+        <div>
+            <div>
+                <Link>
+                    
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Home
