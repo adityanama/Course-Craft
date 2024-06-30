95f2e86c08516b248a55df1326e27fff003c56cd
Aditya Nama <92026058+adityanama@users.noreply.github.com>
2024-07-01 00:25:28 +0530

    final end

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:4000/api/v1"
+const BASE_URL = "https://course-craft-backend.onrender.com/api/v1"
 
 export const endpoints = {
     SENDOTP_API: BASE_URL + "/auth/sendotp",
