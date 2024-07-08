9ab0976387f60fc49acbd4cb1f05693a3251bae2
Aditya Nama <92026058+adityanama@users.noreply.github.com>
2024-07-08 22:07:58 +0530

    "Updated BASE_URL from http to https"

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://course-craft-backend.adityanama.online/api/v1"
+const BASE_URL = "https://course-craft-backend.adityanama.online/api/v1"
 
 export const endpoints = {
     SENDOTP_API: BASE_URL + "/auth/sendotp",
