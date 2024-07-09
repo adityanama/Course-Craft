1af5e3ed20a374ddd05291f95d114cfee92bbd14
Aditya Nama <92026058+adityanama@users.noreply.github.com>
2024-07-09 23:32:57 +0530

    done image

--- src/services/operations/SettingsAPI.js
+++ src/services/operations/SettingsAPI.js
@@ -42,7 +42,9 @@ export const updateProfile = (token, formData) => {
                 throw new Error(response.data.message)
             }
 
-            dispatch(setUser(response.data.updatedUserDetails));
+            const user = response.data.updatedUserDetails;
+            user.image = `https://api.dicebear.com/5.x/initials/svg?seed=${formData.firstName[0]}${formData.lastName[0]}`
+            dispatch(setUser(user));
 
             toast.success(response.data.message);
         }
